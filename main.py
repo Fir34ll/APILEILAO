4744d5ba2ee1a75480b6489e6e6ed84931a7ad8b
Rafael Ramos <106891602+Fir34ll@users.noreply.github.com>
2024-06-07 23:15:30 -0300

    main.py

--- main.py
+++ main.py
@@ -77,4 +77,4 @@ def get_item(item_id: int, db: Session = Depends(get_db)):
     return item
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
